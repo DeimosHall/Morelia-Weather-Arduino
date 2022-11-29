b19eec28e316e733c9f7d219b248e20947764eae
Deimos <deimoshall@gmail.com>
2022-11-29 17:00:30 -0600

    Add the temperature sensor DS18B20 functionallity - Include OneWire and DallasTemperature libraries - Add inputs for an RGB LED - Add a function to read the temperature from the sensor - Add a function to validate if the sensor is working - Add an algorithm to send the temperature every given time in main loop

--- Morelia_Weather.ino
+++ Morelia_Weather.ino
@@ -1,21 +1,35 @@
 #include <WiFi.h>
 #include <FirebaseESP32.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-#define sensor 25
+#define TEMP_SENSOR 33
+#define RGB_RED 25
+#define RGB_GREEN 26
+#define RGB_BLUE 27
+
 #define URL "https://morelia-weather-default-rtdb.firebaseio.com/"
 #define SECRET "5kyFjuSQG2FAh01JluZ1qMQPJg9kw9bRnPofOsTj"
 #define ssid "Pixel_4"
 #define pass "hello123"
 
-float temperature = 0.0;
-
 FirebaseData myFirebaseData;
+OneWire oneWire(TEMP_SENSOR);
+DallasTemperature sensor(&oneWire);
+
+const int SEND_TIME = 10; // Time to send temperature to the database in seconds
+int elapsedTime = 0;
 
 void setup() {
+  pinMode(RGB_RED, OUTPUT);
+  pinMode(RGB_GREEN, OUTPUT);
+  pinMode(RGB_BLUE, OUTPUT);
+  
   Serial.begin(115200);
-  pinMode(sensor, INPUT);
+  sensor.begin(); // Start the DS18B20 sensor
 
   Serial.print("Conecting to network: " + String(ssid));
+  setRed();
   
   WiFi.begin(ssid, pass);
   while(WiFi.status() != WL_CONNECTED) {
@@ -24,16 +38,60 @@ void setup() {
   }
 
   Firebase.begin(URL, SECRET);
-  Firebase.reconnectWiFi(true); // In case of a disconnecton
+  Firebase.reconnectWiFi(true); // In case of a disconnection
   Serial.println("\nConnected to database");  
 }
 
 void loop() {
+  checkSensorStatus();
+
+  Serial.println("Temperature: " + String(getSensorTemp()));
+
+  if (elapsedTime == SEND_TIME) {
+    elapsedTime = 0;
+    sendTemperature(getSensorTemp());
+  } else {
+    elapsedTime++;
+  }
+  
   delay(1000);
-  temperature += 0.1;
-  sendTemperature(temperature);
+}
+
+void checkSensorStatus() {
+  if (getSensorTemp() == -127.0) { // Sensor does not work
+    setGreen();
+  } else {
+    setBlue();
+  }
+}
+
+float getSensorTemp() {
+  sensor.requestTemperatures();
+  return sensor.getTempCByIndex(0);
 }
 
 void sendTemperature(float temperature) {
   Firebase.set(myFirebaseData, "/Temperature", String(temperature));
+}
+
+// Not connected to database
+void setRed() {
+  analogWrite(RGB_RED, 255);
+  analogWrite(RGB_GREEN, 0);
+  analogWrite(RGB_BLUE, 0);
+}
+
+// Connected to database but the sensor does not work
+void setGreen() {
+  analogWrite(RGB_RED, 0);
+  analogWrite(RGB_GREEN, 255);
+  analogWrite(RGB_BLUE, 0);
+}
+
+
+// Connected to database and the sensor works
+void setBlue() {
+  analogWrite(RGB_RED, 0);
+  analogWrite(RGB_GREEN, 0);
+  analogWrite(RGB_BLUE, 255);
 }
